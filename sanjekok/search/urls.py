f243c9918cb6eb047a75b13bb734c31af37a3851
htsgts <davidmoon824@naver.com>
2025-12-01 23:29:56 +0900

    search 기능 추가

--- sanjekok/search/urls.py
+++ sanjekok/search/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from . import views
 
-app_name = 'Search'
+app_name = "search"
 
 urlpatterns = [
+    # 주변 산재 검색
+    path("accident/", views.search_accident, name="search_accident"),
 
+    # 주변 병원 검색
+    path("hospital/", views.search_hospital, name="search_hospital"),
+
+    # 병원 상세 페이지
+    path("hospital/<int:hospital_id>/", views.hospital_detail, name="hospital_detail"),
 ]
--- sanjekok/search/views.py
+++ sanjekok/search/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+# 주변 산재 검색
+@login_required(login_url="/login/")
+def search_accident(request):
+    return render(request, "search_accident.html")
+
+
+# 주변 병원 검색
+@login_required(login_url="/login/")
+def search_hospital(request):
+    return render(request, "search_hospital.html")
+
+
+# 병원 상세 페이지
+@login_required(login_url="/login/")
+def hospital_detail(request, hospital_id):
+    context = {
+        "hospital_id": hospital_id,   # 템플릿에서 id 사용 가능
+    }
+    return render(request, "hospital_detail.html", context)
