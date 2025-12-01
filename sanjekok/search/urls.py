1a0a83e24128547e46846d853a43441fc3df4a55
htsgts <davidmoon824@naver.com>
2025-12-02 00:08:34 +0900

    search 부분 수정

--- sanjekok/search/urls.py
+++ sanjekok/search/urls.py
@@ -5,11 +5,5 @@ app_name = "search"
 
 urlpatterns = [
     # 주변 산재 검색
-    path("accident/", views.search_accident, name="search_accident"),
-
-    # 주변 병원 검색
-    path("hospital/", views.search_hospital, name="search_hospital"),
-
-    # 병원 상세 페이지
-    path("hospital/<int:hospital_id>/", views.hospital_detail, name="hospital_detail"),
+    path("search/", views.search),
 ]
--- sanjekok/search/views.py
+++ sanjekok/search/views.py
@@ -3,21 +3,7 @@ from django.contrib.auth.decorators import login_required
 
 
 # 주변 산재 검색
-@login_required(login_url="/login/")
-def search_accident(request):
-    return render(request, "search_accident.html")
+# @login_required(login_url="/login/")
+def search(request):
+    return render(request, "search.html")
 
-
-# 주변 병원 검색
-@login_required(login_url="/login/")
-def search_hospital(request):
-    return render(request, "search_hospital.html")
-
-
-# 병원 상세 페이지
-@login_required(login_url="/login/")
-def hospital_detail(request, hospital_id):
-    context = {
-        "hospital_id": hospital_id,
-    }
-    return render(request, "hospital_detail.html", context)
