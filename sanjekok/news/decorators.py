2e539b1608a3e2403377f786bb2594234c6934a7
soyoonpk <sounpk514@gmail.com>
2025-12-18 11:13:04 +0900

    크롤링admin만

--- sanjekok/news/decorators.py
+++ sanjekok/news/decorators.py
@@ -1,17 +1,6 @@
 from functools import wraps
 from django.shortcuts import redirect
 from django.contrib import messages
-
-def login_required(function):
-    @wraps(function)
-    def wrap(request, *args, **kwargs):
-        if 'member_id' not in request.session:
-            messages.error(request, "로그인이 필요합니다.")
-            return redirect('Member:login')
-        return function(request, *args, **kwargs)
-    return wrap
-
-
 from member.models import Member
 
 def crawl_admin_required(function):
